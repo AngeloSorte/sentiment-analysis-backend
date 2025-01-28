727d1c684d20ca44361d3183a45fe467bbdad0bc
Angelo <33905143+AngeloSorte@users.noreply.github.com>
2025-01-28 10:31:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ from flask_cors import CORS
 
 # Inizializza Flask app
 app = Flask(__name__)
+# Configura il CORS per consentire richieste dal tuo dominio Wix
+CORS(app, resources={r"/predict": {"origins": "https://truthseekers1983-wixstudio-com.filesusr.com"}})
 
-CORS(app, origins=["https://truthseekers1983.wixstudio.com/aaitextgenerator/blank-1"])
 
 # Carica il modello salvato
 model = load_model('sentiment_analysis_model.h5')
